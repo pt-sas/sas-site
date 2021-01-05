e30e7202a56f154f0f6c4a7dd918f5d39c41369a
Tintapen <Oki@HPDQ1037-OKI.SAS1.local>
2021-01-05 20:20:00 +0700

    first commit partials template

---
+++ app/Views/admin/_partials/js.php
@@ -0,0 +1,45 @@
+<!-- jQuery -->
+<script src="<?= base_url('assets/plugins/jquery/jquery.min.js') ?>"></script>
+<!-- jQuery UI 1.11.4 -->
+<!-- <script src="<?//= base_url('assets/plugins/jquery-ui/jquery-ui.min.js ') ?>"></script> -->
+<!-- Bootstrap 4 -->
+<script src="<?= base_url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+<!-- daterangepicker -->
+<script src="<?= base_url('assets/plugins/moment/moment.min.js') ?>"></script>
+<script src="<?= base_url('assets/plugins/daterangepicker/daterangepicker.js') ?>"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="<?= base_url('assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') ?>"></script>
+<!-- overlayScrollbars -->
+<script src="<?= base_url('assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') ?>"></script>
+<!-- Select2 -->
+<script src="<?= base_url('assets/plugins/select2/js/select2.full.min.js') ?>"></script>
+<!-- bs-custom-file-input -->
+<script src="<?= base_url('assets/plugins/bs-custom-file-input/bs-custom-file-input.min.js') ?>"></script>
+<!-- SweetAlert2 -->
+<script src="<?= base_url('assets/plugins/sweetalert2/sweetalert2.min.js') ?>"></script>
+<!-- AdminLTE App -->
+<script src="<?= base_url('assets/dist/js/adminlte.js') ?>"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?= base_url('assets/dist/js/demo.js') ?>"></script>
+<!-- fullCalendar 2.2.5 -->
+<script src="<?= base_url('assets/plugins/moment/moment.min.js') ?>"></script>
+<script src="<?= base_url('assets/plugins/fullcalendar/main.min.js') ?>"></script>
+<script src="<?= base_url('assets/plugins/fullcalendar-daygrid/main.min.js') ?>"></script>
+<script src="<?= base_url('assets/plugins/fullcalendar-timegrid/main.min.js') ?>"></script>
+<script src="<?= base_url('assets/plugins/fullcalendar-interaction/main.min.js') ?>"></script>
+<script src="<?= base_url('assets/plugins/fullcalendar-bootstrap/main.min.js') ?>"></script>
+<!-- DataTables -->
+<script src="<?= base_url('assets/plugins/datatables/jquery.dataTables.js') ?>"></script>
+<script src="<?= base_url('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.js') ?>"></script>
+<script src="<?= base_url('assets/plugins/datatables-fixedcolumns/js/dataTables.fixedColumns.min.js') ?>"></script>
+<!-- bootstrap-datepicker -->
+<!-- <script src="<?php //echo base_url('assets/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js') 
+                    ?>"></script> -->
+<!-- Bootstrap Toggle -->
+<!-- <script src="<?//= base_url('assets/plugins/bootstrap4-toggle/js/bootstrap4-toggle.min.js') ?>"></script> -->
+<!-- AutoNumeric Rupiah -->
+<script src="<?= base_url('assets/plugins/auto-numeric/autoNumeric.js') ?>"></script>
+
+<script src="<?= base_url('assets/cust/js/Process.js') ?>"></script>
+<script src="<?= base_url('assets/cust/js/Table.js') ?>"></script>
+<script src="<?= base_url('assets/cust/js/Product.js') ?>"></script>
