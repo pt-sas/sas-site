7b6c42f9e6d5ec2f160ac885a03d7103526be000
Tintapen <okiper12@gmail.com>
2021-01-07 09:59:54 +0700

    add view bank

---
+++ app/Views/admin/bank/form_bank.php
@@ -0,0 +1,44 @@
+<div class="modal fade modal_form form">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title"></h4>
+                <button type="button" class="close x_form" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form class="form-horizontal form_open" id="form_bank">
+                    <?= csrf_field(); ?>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="bnk_name">Name <span class="required">*</span></label>
+                                    <input type="text" class="form-control" id="bnk_name" name="bnk_name" placeholder="Enter bank name">
+                                    <small class="form-text text-danger" id="error_bnk_name"></small>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="bnk_desc">Description</label>
+                                    <textarea class="form-control" id="bnk_desc" name="bnk_desc" rows="2"></textarea>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="custom-control custom-checkbox">
+                                    <input type="checkbox" class="custom-control-input active" id="bnk_isactive" name="bnk_isactive">
+                                    <label for="bnk_isactive" class="custom-control-label">Active</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-outline-danger close_form" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-outline-primary save_form">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
