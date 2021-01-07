c1f319492b7ea023aa44ff59b4ab679090fa099c
Tintapen <okiper12@gmail.com>
2021-01-07 10:05:35 +0700

    modified views menu

--- app/Views/admin/menu/form_menu.php
+++ app/Views/admin/menu/form_menu.php
@@ -1,31 +1,39 @@
-<div class="modal fade modal_form">
+<div class="modal fade modal_form form">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
                 <h4 class="modal-title"></h4>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <button type="button" class="close x_form" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <form class="form-horizontal" id="form_menu">
+                <form class="form-horizontal form_open" id="form_menu">
+                    <?= csrf_field(); ?>
                     <div class="card-body">
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <label for="pro_code">Code Product <span class="required">*</span></label>
-                                    <input type="text" class="form-control" id="pro_code" name="pro_code" placeholder="Enter code product">
-                                    <small id="error_pro_code" class="form-text text-danger"></small>
+                                    <label for="mnu_name">Menu <span class="required">*</span></label>
+                                    <input type="text" class="form-control" id="mnu_name" name="mnu_name" placeholder="Enter menu name">
+                                    <small class="form-text text-danger" id="error_mnu_name"></small>
                                 </div>
+                            </div>
+                            <div class="col-md-6">
                                 <div class="form-group">
-                                    <label for="pro_name">Product <span class="required">*</span></label>
-                                    <div class="input-group">
-                                        <input type="text" class="form-control" id="pro_name" name="pro_name" placeholder="Enter product name">
-                                        <div class="input-group-append">
-                                            <span class="input-group-text"><i class="fas fa-tshirt"></i></span>
-                                        </div>
-                                    </div>
-                                    <small id="error_pro_name" class="form-text text-danger"></small>
+                                    <label for="mnu_status">Status <span class="required">*</span></label>
+                                    <select class="form-control select2" id="mnu_status" name="mnu_status">
+                                        <option value="">--Select Menu</option>
+                                        <option value="F">Front End</option>
+                                        <option value="B">Back End</option>
+                                    </select>
+                                    <small id="error_mnu_status" class="form-text text-danger"></small>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="custom-control custom-checkbox">
+                                    <input type="checkbox" class="custom-control-input active" id="mnu_isactive" name="mnu_isactive">
+                                    <label for="mnu_isactive" class="custom-control-label">Active</label>
                                 </div>
                             </div>
                         </div>
--- app/Views/admin/menu/v_menu.php
+++ app/Views/admin/menu/v_menu.php
@@ -2,13 +2,28 @@
 
 <?= $this->section('content'); ?>
 <?= $this->include('admin/menu/form_menu'); ?>
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-lg-3 col-6">
-            <p>Test</p>
-            <button type="button" class="btn bg-gradient-primary btn-sm" title="New Product" id="new_menu"><i class=" fas fa-plus-circle"> New</i></button>
-        </div>
-        <div class="col-lg-3 col-6">
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <div class="float-left">
+                    <button type="button" class="btn bg-gradient-primary btn-sm new_form" title="New Menu"><i class=" fas fa-plus-circle"> New</i></button>
+                </div>
+            </div>
+            <div class="card-body">
+                <table class="table table-bordered table-hover table-pointer tb_display table-md" style="width: 100%">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>#</th>
+                            <th>Menu</th>
+                            <th>Status</th>
+                            <th>Active</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
         </div>
     </div>
 </div>
