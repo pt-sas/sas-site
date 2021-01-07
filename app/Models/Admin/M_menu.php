9f71ae03ab576f6e7aff6b5c8f94465e65b3fac6
Tintapen <okiper12@gmail.com>
2021-01-07 10:04:47 +0700

    modified models menu

---
+++ app/Models/Admin/M_menu.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models\Admin;
+
+use CodeIgniter\Model;
+
+class M_menu extends Model
+{
+    protected $table      = 'sys_menu';
+    protected $primaryKey = 'menu_id';
+    protected $allowedFields = [
+        'name',
+        'status',
+        'isactive'
+    ];
+    protected $useTimestamps = true;
+
+
+    public function formError()
+    {
+        $validation = \Config\Services::validation();
+
+        return [
+            [
+                'error'        =>   true,
+                'field'        =>   'menu'
+            ],
+            [
+                'error'        =>   'error_mnu_name',
+                'field'        =>   'mnu_name',
+                'label'        =>   $validation->getError('mnu_name')
+            ],
+            [
+                'error'        =>   'error_mnu_status',
+                'field'        =>   'mnu_status',
+                'label'        =>   $validation->getError('mnu_status')
+            ]
+        ];
+    }
+}
