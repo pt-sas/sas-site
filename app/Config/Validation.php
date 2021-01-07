bb0ac3e4fab25b9cf5b02514b7e8bf92c3885f5c
Tintapen <okiper12@gmail.com>
2021-01-07 10:06:59 +0700

    new variable validation

--- app/Config/Validation.php
+++ app/Config/Validation.php
@@ -78,6 +78,84 @@ class Validation
 		'pro_volume' => [
 			'label'		=> 'Volume',
 			'rules'		=> 'required'
+		]
+	];
+
+	public $menu = [
+		'mnu_name' => [
+			'label'		=> 'Name',
+			'rules' 	=> 'required|is_unique[sys_menu.name,menu_id,{id}]',
+			'errors' 	=> [
+				'is_unique' => 'This {field} already exists.'
+			]
 		],
+		'mnu_status' => [
+			'label'		=> 'Status',
+			'rules'		=> 'required'
+		]
+	];
+
+	public $submenu = [
+		'sub_name' => [
+			'label'		=> 'Name',
+			'rules' 	=> 'required|is_unique[sys_submenu.name,sys_submenu_id,{id}]',
+			'errors' 	=> [
+				'is_unique' => 'This {field} already exists.'
+			]
+		],
+		'sub_status' => [
+			'label'		=> 'Status',
+			'rules'		=> 'required'
+		]
+	];
+
+	public $uom = [
+		'uom_name' => [
+			'label'		=> 'Name',
+			'rules' 	=> 'required|is_unique[md_uom.name,md_uom_id,{id}]',
+			'errors' 	=> [
+				'is_unique' => 'This {field} already exists.'
+			]
+		]
+	];
+
+	public $bank = [
+		'bnk_name' => [
+			'label'		=> 'Name',
+			'rules' 	=> 'required|is_unique[md_bank.name,md_bank_id,{id}]',
+			'errors' 	=> [
+				'is_unique' => 'This {field} already exists.'
+			]
+		]
+	];
+
+	public $greeting = [
+		'gre_name' => [
+			'label'		=> 'Name',
+			'rules' 	=> 'required|is_unique[md_greeting.name,md_greeting_id,{id}]',
+			'errors' 	=> [
+				'is_unique' => 'This {field} already exists.'
+			]
+		]
+	];
+
+	public $division = [
+		'div_name' => [
+			'label'		=> 'Name',
+			'rules' 	=> 'required|is_unique[md_division.name,md_division_id,{id}]',
+			'errors' 	=> [
+				'is_unique' => 'This {field} already exists.'
+			]
+		]
+	];
+
+	public $principal = [
+		'pri_name' => [
+			'label'		=> 'Name',
+			'rules' 	=> 'required|is_unique[md_principal.name,md_principal_id,{id}]',
+			'errors' 	=> [
+				'is_unique' => 'This {field} already exists.'
+			]
+		]
 	];
 }
