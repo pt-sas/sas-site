06577f42ba85255e8eca420f0729d5970ae6afd2
Tintapen <okiper12@gmail.com>
2021-01-12 10:26:16 +0700

    set uri segment on breadcrumb based on url

--- app/Views/admin/_partials/breadcrumb.php
+++ app/Views/admin/_partials/breadcrumb.php
@@ -1,14 +1,21 @@
+<?php $request = \Config\Services::request(); ?>
 <!-- Content Header (Page header) -->
 <div class="content-header">
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="m-0 text-dark">Dashboard</h1>
+                <?php if (empty($request->uri->getSegment(2))) : ?>
+                    <h1 class="m-0 text-dark">Home</h1>
+                <?php else : ?>
+                    <h1 class="m-0 text-dark"><?= ucfirst($request->uri->getSegment(2)); ?></h1>
+                <?php endif ?>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active">Dashboard</li>
+                    <?php if (!empty($request->uri->getSegment(2))) : ?>
+                        <li class="breadcrumb-item"><a href="<?= site_url('/admin') ?>">Home</a></li>
+                        <li class="breadcrumb-item active"><?= ucfirst($request->uri->getSegment(2)); ?></li>
+                    <?php endif ?>
                 </ol>
             </div>
         </div><!-- /.row -->
