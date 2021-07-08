9d7b3af1c7f682316128925042cee8c7fdd07c7e
Tintapen <okiper12@gmail.com>
2021-07-08 20:25:05 +0700

    Update content to change position

--- app/Views/backend/_partials/overview.php
+++ app/Views/backend/_partials/overview.php
@@ -1,37 +1,57 @@
 <!DOCTYPE html>
 <html>
-  <?= $this->include('backend/_partials/head') ?>
-  <body>
-    <div class="wrapper">
-      <div class="main-header">
-        <?= $this->include('backend/_partials/logo') ?>
-        <?= $this->include('backend/_partials/navbar') ?>
-      </div>
-      <?= $this->include('backend/_partials/sidebar') ?>
+<?= $this->include('backend/_partials/head') ?>
 
-      <div class="main-panel">
-        <?php if($title == 'Mailbox') : ?>
-    			<div class="container container-full">
-    				<div class="page-inner page-inner-fill">
-              <?= $this->renderSection('content') ?>
+<body>
+  <div class="wrapper">
+    <div class="main-header">
+      <?= $this->include('backend/_partials/logo') ?>
+      <?= $this->include('backend/_partials/navbar') ?>
+    </div>
+    <?= $this->include('backend/_partials/sidebar') ?>
+
+    <div class="main-panel">
+      <?php if ($title == 'Mailbox') : ?>
+        <div class="container container-full">
+          <div class="page-inner page-inner-fill">
+            <?= $this->renderSection('content') ?>
+          </div>
+        </div>
+      <?php else : ?>
+        <div class="container">
+          <div class="page-inner">
+            <?= $this->include('backend/_partials/breadcrumb') ?>
+            <div class="row main_page">
+              <div class="col-md-12">
+                <div class="card">
+                  <div class="card-header">
+                    <div class="d-flex align-items-center">
+                      <h4 class="card-title"><?= $title; ?></h4>
+                      <button type="button" class="btn btn-primary btn-sm btn-round ml-auto new_form" title="Location">
+                        <i class="fa fa-plus fa-fw"></i> Location
+                      </button>
+                    </div>
+                  </div>
+                  <?= $this->renderSection('content') ?>
+                  <div class="card-action card-button">
+                    <button type="button" class="btn btn-outline-danger btn-round ml-auto close_form">Close</button>
+                    <button type="button" class="btn btn-primary btn-round ml-auto save_form">Save changes</button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        <?php else : ?>
-          <div class="container">
-            <div class="page-inner">
-              <?= $this->include('backend/_partials/breadcrumb') ?>
-              <?= $this->renderSection('content') ?>
-            </div>
-    			</div>
-        <?php endif ; ?>
-        <?= $this->include('backend/_partials/footer') ?>
-      </div>
-
-      <?= $this->include('backend/_partials/quicksidebar') ?>
+        </div>
+      <?php endif; ?>
+      <?= $this->include('backend/_partials/footer') ?>
     </div>
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
-    <?= $this->include('backend/_partials/js') ?>
-  </body>
+
+    <?= $this->include('backend/_partials/quicksidebar') ?>
+  </div>
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+  <?= $this->include('backend/_partials/js') ?>
+</body>
+
 </html>
