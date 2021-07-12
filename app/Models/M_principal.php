841ec723c74043b87e7c72342910a6a012ad3e07
Tintapen <okiper12@gmail.com>
2021-07-12 15:35:42 +0700

    New models principal

--- app/Models/M_principal.php
+++ app/Models/M_principal.php
@@ -4,32 +4,17 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class M_principal extends Model
+class M_Principal extends Model
 {
-	protected $table                = 'md_principal';
-	protected $primaryKey           = 'md_principal_id';
-	protected $allowedFields        = ['name', 'description', 'url', 'md_image_id', 'isactive'];
-	protected $returnType           = 'App\Entities\Principal';
-	protected $useTimestamps        = true;
-
-	public function detail($field, $where = null)
-	{
-		$db = \Config\Database::connect();
-		if (!empty($where)) {
-			return $db->query("SELECT
-						mdp.md_principal_id,
-						mdp.isactive,
-						mdp.name,
-						mdp.description,
-						mdi.name as image,
-						mdi.image_url as md_image_id,
-						mdp.url,
-						mdp.md_image_id as image_id
-						FROM $this->table mdp
-						LEFT JOIN Md_Image mdi ON mdp.Md_Image_ID = mdi.Md_Image_ID
-						WHERE $field = $where");
-		} else {
-			return $where;
-		}
-	}
+    protected $table      = 'md_principal';
+    protected $primaryKey = 'md_principal_id';
+    protected $allowedFields = [
+        'name',
+        'description',
+        'logo',
+        'url',
+        'isactive'
+    ];
+    protected $useTimestamps = true;
+    protected $returnType = 'App\Entities\Principal';
 }
