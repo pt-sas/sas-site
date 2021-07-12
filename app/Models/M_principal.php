04643149a115a3c704e8207d471230872c47eeb8
Tintapen <okiper12@gmail.com>
2021-07-12 16:40:26 +0700

    New model principal

---
+++ app/Models/M_principal.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class M_Principal extends Model
+{
+	protected $table      = 'md_principal';
+	protected $primaryKey = 'md_principal_id';
+	protected $allowedFields = [
+		'name',
+		'description',
+		'logo',
+		'url',
+		'isactive'
+	];
+	protected $useTimestamps = true;
+	protected $returnType = 'App\Entities\Principal';
+
+	public function detail($field, $where = null)
+	{
+		$db = \Config\Database::connect();
+		if (!empty($where)) {
+			return $db->query("SELECT
+						mdp.md_principal_id,
+						mdp.isactive,
+						mdp.name,
+						mdp.description,
+						mdi.name as image,
+						mdi.image_url as md_image_id,
+						mdp.url,
+						mdp.md_image_id as image_id
+						FROM $this->table mdp
+						LEFT JOIN Md_Image mdi ON mdp.Md_Image_ID = mdi.Md_Image_ID
+						WHERE $field = $where");
+		} else {
+			return $where;
+		}
+	}
+}
