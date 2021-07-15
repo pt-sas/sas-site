110647bf6aa01c7d6d610ffba500fb071cd718cc
Tintapen <okiper12@gmail.com>
2021-07-16 01:54:56 +0700

    Add column image

--- app/Controllers/Backend/News.php
+++ app/Controllers/Backend/News.php
@@ -41,6 +41,7 @@ class News extends BaseController
 			$row[] = $ID;
 			$row[] = $number;
 			$row[] = $value->title;
+			$row[] = $this->picture->render($this->table, $this->path_folder, 'md_image_id', $value->md_image_id);
 			$row[] = $value->news_date;
 			$row[] = active($value->isactive);
 			$row[] = '<center>
