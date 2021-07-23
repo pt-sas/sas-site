41a7d0dec2cba9cd5cbf3db18c16f4e642c1ff3a
Anhar Panduwinata <anharpanduwinata@gmail.com>
2021-07-23 14:49:39 +0700

    Commit 10

--- app/Language/en/Home.php
+++ app/Language/en/Home.php
@@ -4,17 +4,17 @@
 // Contoh, We Are Looking For pada Career, cH421, Page Career, Syntax H4, Kolom ke 2 pada Page Career, Urutan 1 diantara Syntax yang sama pada Kolom tsb
 
 return [
-    'HH211' => 'Beyond Authorized Distributor',
-    'HP11' => 'Bring together a human approach with advanced technology to create an efficient and effective office space.',
+    'HH211' => 'Authorized Distributor',
+    'HP11' => 'One stop solution for all your electrical needs',
 
     'HH321' => 'Why us?',
     'HP21' => 'We experienced in Lighting and Electrical Equipment in Indonesia',
     'HH421' => 'Quality Product',
     'HHP21' => 'We only deliver quality products that you can trust and rely on – no compromises on this one!',
-    'HH422' => 'Distribution Network',
-    'HHP22' => 'Our business partners are very accessible and always ready to serve you wherever you are.',
-    'HH423' => 'Professional Team',
-    'HHP23' => 'Our knowledgeable sales professionals are only a call away to immediately assist you and understand your needs.',
+    'HH422' => 'Excellent Service',
+    'HHP22' => 'From ordering to delivery, our team members strive to provide you with a pleasent and seamless shopping experience.',
+    'HH423' => 'Comprehensive Distribution Network',
+    'HHP23' => 'We have an expansive coverage area and products that are accessible wherever you are.',
 
     'HH331' => 'Explore more our brand partners',
     'HP31' => 'Quis fringilla egestas dis viverra semper commodo. Massa vivamus accumsan imperdiet et. Ut urna tempor aliquet elementum eget ultrices vitae. Convallis vulputate at malesuada orci morbi. Ut elementum nunc justo orci, sagittis mauris vestibulum purus.',
