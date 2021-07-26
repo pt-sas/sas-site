d2f5a86285fe748f78bdf98ec7a68d93393d70a6
Tintapen <okiper12@gmail.com>
2021-07-26 15:32:20 +0700

    Add function showProductBy based on principal, category1, category2, category3

--- app/Models/M_Product.php
+++ app/Models/M_Product.php
@@ -90,4 +90,37 @@ class M_Product extends Model
 		$query = $builder->get()->getResult();
 		return $query;
 	}
+
+	public function showProductBy($principal = null, $category1 = null, $category2 = null, $category3 = null)
+	{
+		$db = \Config\Database::connect();
+		$builder = $db->table('md_product p');
+		$builder->select('p.code,
+						p.name,
+						p.description,
+						p.url');
+		$builder->distinct();
+		$builder->join('md_productcategory pc', 'p.md_product_id = pc.md_product_id', 'left');
+		$builder->join('md_principal pr', 'pr.md_principal_id = pc.md_principal_id', 'left');
+		$builder->where('p.isactive', 'Y');
+		if (!empty($principal)) {
+			$builder->where('pr.url', $principal);
+		}
+
+		if (!empty($category1)) {
+			$builder->where('pc.category1', $category1);
+		}
+
+		if (!empty($category2)) {
+			$builder->where('pc.category2', $category2);
+		}
+
+		if (!empty($category3)) {
+			$builder->where('pc.category3', $category3);
+		}
+
+		$builder->orderBy('p.code', 'ASC');
+		$query = $builder->get();
+		return $query;
+	}
 }
