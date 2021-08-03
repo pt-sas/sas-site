d55a2c5cbc91c53cc73dcc0a5ad9c432feb68d36
Tintapen <okiper12@gmail.com>
2021-08-03 16:44:14 +0700

    Set query builder filter job and set where clause expired date

--- app/Models/M_Job.php
+++ app/Models/M_Job.php
@@ -31,27 +31,33 @@ class M_Job extends Model
     $builder = $db->table('trx_job');
     $builder->select('md_division.name as division_name, trx_job_id, position, level, trx_job.description, trx_job.requirement, trx_job.description_en, trx_job.requirement_en, posted_date, expired_date, trx_job.isactive, trx_job.url');
     $builder->join('md_division', 'md_division.md_division_id = trx_job.md_division_id', 'left');
+    $builder->where('trx_job.isactive', 'Y');
+    $builder->where('DATE(trx_job.expired_date)', 'CURDATE()', false);
     $query = $builder->get()->getResult();
     return $query;
   }
 
-	public function showPositionBy($level = null, $keyword = null)
-	{
-		$db = \Config\Database::connect();
-    $builder = $db->table('trx_job');
-    $builder->select('md_division.name as division_name, trx_job_id, position, level, trx_job.description, trx_job.requirement, trx_job.description_en, trx_job.requirement_en, posted_date, expired_date, trx_job.isactive, trx_job.url');
-    $builder->join('md_division', 'md_division.md_division_id = trx_job.md_division_id', 'left');
-		$builder->where('isactive', 'Y');
-		if (!empty($keyword)) {
-			$builder->like('position', $keyword, 'both');
-			$builder->like('description', $keyword, 'both');
-		}
-		if (!empty($level)) {
-			$builder->where('level', $level);
-		}
+  public function showPositionBy($level = null, $keyword = null)
+  {
+    $db = \Config\Database::connect();
+    $builder = $db->table('trx_job as job');
+    $builder->select('job.*,
+                    d.name as division_name');
+    $builder->join('md_division d', 'd.md_division_id = job.md_division_id', 'left');
+    $builder->where('job.isactive', 'Y');
+    $builder->where('DATE(job.expired_date)', 'CURDATE()', false);
 
-		$builder->orderBy('posted_date', 'DESC');
-		$query = $builder->get();
-		return $query;
-	}
+    if (!empty($keyword)) {
+      $builder->like('job.position', $keyword, 'both');
+      $builder->like('job.description', $keyword, 'both');
+    }
+
+    if (!empty($level)) {
+      $builder->where('job.level', $level);
+    }
+
+    $builder->orderBy('posted_date', 'DESC');
+    $query = $builder->get();
+    return $query;
+  }
 }
