41c6475169b23b67ce39df2f9fcac03a7c8345be
Tintapen <okiper12@gmail.com>
2021-08-04 14:02:06 +0700

    Add column version and mobile

--- app/Models/M_Visit.php
+++ app/Models/M_Visit.php
@@ -6,34 +6,38 @@ use CodeIgniter\Model;
 
 class M_Visit extends Model
 {
-    protected $table      = 'sys_visit';
-    protected $primaryKey = 'id';
-    protected $allowedFields = [
-        'ipaddress',
-        'browser',
-        'platform',
-        'time',
-    ];
-    protected $useTimestamps = true;
-    protected $returnType = 'App\Entities\Visit';
+  protected $table      = 'sys_visit';
+  protected $primaryKey = 'id';
+  protected $allowedFields = [
+    'ipaddress',
+    'browser',
+    'platform',
+    'time',
+  ];
+  protected $useTimestamps = true;
+  protected $returnType = 'App\Entities\Visit';
 
 
-    public function count($ipaddress) {
-        $db = \Config\Database::connect();
-        $agent = \Config\Services::request()->getUserAgent();
+  public function count($ipaddress)
+  {
+    $db = \Config\Database::connect();
+    $agent = \Config\Services::request()->getUserAgent();
 
-        $builder = $db->table('sys_visit');
-        $builder->where(['ipaddress' => $ipaddress]);
-        $builder->where('created_at BETWEEN DATE_SUB(NOW(), INTERVAL 2 HOUR) AND NOW()');
-        $query = $builder->countAllResults();
-        if ($query == 0) {
-          $data = [
-            'ipaddress' => $ipaddress,
-            'browser' 	=> $agent->getBrowser(),
-            'platform' 	=> $agent->getPlatform(),
-            'time'      => date('Y-m-j H:i:s')
-      		];
-      		$builder->insert($data);
-        }
-  	}
+    $builder = $db->table('sys_visit');
+    $builder->where(['ipaddress' => $ipaddress]);
+    $builder->where('created_at BETWEEN DATE_SUB(NOW(), INTERVAL 2 HOUR) AND NOW()');
+    $query = $builder->countAllResults();
+
+    if ($query == 0) {
+      $data = [
+        'ipaddress' => $ipaddress,
+        'browser'   => $agent->getBrowser(),
+        'version'   => $agent->getVersion(),
+        'platform'  => $agent->getPlatform(),
+        'mobile'    => $agent->getMobile(),
+        'time'      => date('Y-m-j H:i:s')
+      ];
+      $builder->insert($data);
+    }
+  }
 }
