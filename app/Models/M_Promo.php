a3ec6299f472c8b8a002d0f530425194b2b1886d
Tintapen <okiper12@gmail.com>
2021-08-06 20:10:45 +0700

    Change name table and clause join

--- app/Models/M_Promo.php
+++ app/Models/M_Promo.php
@@ -6,27 +6,27 @@ use CodeIgniter\Model;
 
 class M_Promo extends Model
 {
-    protected $table      = 'trx_promo';
-    protected $primaryKey = 'trx_promo_id';
-    protected $allowedFields = [
-        'md_image_id',
-        'title',
-        'content',
-    		'title_en',
-    		'content_en',
-        'start_date',
-        'end_date',
-        'slug',
-        'isactive'
-    ];
-    protected $useTimestamps = true;
-    protected $returnType = 'App\Entities\News';
+	protected $table      = 'trx_promo';
+	protected $primaryKey = 'trx_promo_id';
+	protected $allowedFields = [
+		'md_image_id',
+		'title',
+		'content',
+		'title_en',
+		'content_en',
+		'start_date',
+		'end_date',
+		'slug',
+		'isactive'
+	];
+	protected $useTimestamps = true;
+	protected $returnType = 'App\Entities\News';
 
-  	public function detail($field, $where = null)
-  	{
-  		$db = \Config\Database::connect();
-  		if (!empty($where)) {
-  			return $db->query("SELECT
+	public function detail($field, $where = null)
+	{
+		$db = \Config\Database::connect();
+		if (!empty($where)) {
+			return $db->query("SELECT
   						mdp.trx_promo_id ,
   						mdp.isactive,
   						mdp.title,
@@ -40,31 +40,31 @@ class M_Promo extends Model
   						mdi.image_url as md_image_id,
   						mdp.md_image_id as image_id
   						FROM $this->table mdp
-  						LEFT JOIN Md_Image mdi ON mdp.Md_Image_ID = mdi.Md_Image_ID
+  						LEFT JOIN md_image mdi ON mdp.md_image_id = mdi.md_image_id
   						WHERE $field = $where");
-  		} else {
-  			return $where;
-  		}
-  	}
+		} else {
+			return $where;
+		}
+	}
 
-    public function showAll()
-    {
-      $db = \Config\Database::connect();
-      $builder = $db->table('trx_promo');
-      $builder->select('title, content, title_en, content_en, start_date, end_date, slug, image_url');
-      $builder->join('md_image', 'md_image.md_image_id = trx_promo.md_image_id');
-      $query = $builder->get()->getResult();
-      return $query;
-    }
+	public function showAll()
+	{
+		$db = \Config\Database::connect();
+		$builder = $db->table('trx_promo');
+		$builder->select('title, content, title_en, content_en, start_date, end_date, slug, image_url');
+		$builder->join('md_image', 'md_image.md_image_id = trx_promo.md_image_id');
+		$query = $builder->get()->getResult();
+		return $query;
+	}
 
-  	public function getDetail($slug)
-  	{
-  		$db = \Config\Database::connect();
-  		$builder = $db->table('trx_promo');
-  		$builder->select('title, content, title_en, content_en, image_url, start_date, end_date');
-  		$builder->join('md_image', 'md_image.md_image_id = trx_promo.md_image_id');
-      $builder->where('slug',$slug);
-  		$query = $builder->get()->getRow();
-  		return $query;
-  	}
+	public function getDetail($slug)
+	{
+		$db = \Config\Database::connect();
+		$builder = $db->table('trx_promo');
+		$builder->select('title, content, title_en, content_en, image_url, start_date, end_date');
+		$builder->join('md_image', 'md_image.md_image_id = trx_promo.md_image_id');
+		$builder->where('slug', $slug);
+		$query = $builder->get()->getRow();
+		return $query;
+	}
 }
