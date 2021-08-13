8944255f672646c7a2f9f6f83237cf8825df58b7
Tintapen <okiper12@gmail.com>
2021-08-13 09:06:58 +0700

    Update controller product use datatable server side

--- app/Controllers/Backend/Product.php
+++ app/Controllers/Backend/Product.php
@@ -4,7 +4,11 @@ namespace App\Controllers\Backend;
 
 use App\Controllers\BaseController;
 use App\Models\M_Product;
-use App\Models\M_Image;
+use App\Models\M_Principal;
+use App\Models\M_Category;
+use App\Models\M_Uom;
+
+use Config\Services;
 
 class Product extends BaseController
 {
@@ -16,89 +20,113 @@ class Product extends BaseController
 		$this->new_title = 'Product';
 		$this->form_type = 'new_form';
 
+		$principal = new M_Principal();
+		$uom = new M_Uom();
+
 		$data = [
 			'title'    	=> '' . $this->new_title . '',
 			'button'    => '<button type="button" class="btn btn-primary btn-sm btn-round ml-auto ' . $this->form_type . ' ' . $this->modal_type . '" title="' . $this->new_title . '">
 												<i class="fa fa-plus fa-fw"></i> ' . $this->new_title . '
 										 </button>',
+			'principal'	=> $principal->where('isactive', 'Y')
+				->orderBy('name', 'ASC')
+				->findAll(),
+			'uom'	=> $uom->where('isactive', 'Y')
+				->orderBy('name', 'ASC')
+				->findAll()
 		];
 		return $this->template->render('backend/product/v_product', $data);
 	}
 
 	public function showAll()
 	{
-		$product = new M_Product();
-		$list = $product->findAll();
-		$data = [];
+		$request = Services::request();
+		$product = new M_Product($request);
 
-		$number = 0;
-		foreach ($list as $value) :
-			$row = [];
-			$ID = $value->md_product_id;
+		if ($request->getMethod(true) === 'POST') {
+			$lists = $product->getDatatables();
+			$data = [];
+			$number = $request->getPost('start');
 
-			$number++;
+			foreach ($lists as $value) :
+				$row = [];
+				$ID = $value->md_product_id;
 
-			$row[] = $ID;
-			$row[] = $number;
-			$row[] = $value->title;
-			$row[] = $this->picture->render($this->table, $this->path_folder, 'md_image_id', $value->md_image_id);
-			$row[] = $value->product_date;
-			$row[] = active($value->isactive);
-			$row[] = '<center>
+				$number++;
+
+				$row[] = $ID;
+				$row[] = $number;
+				$row[] = $value->code;
+				$row[] = $value->name;
+				$row[] = $this->picture->render($this->path_folder, $value->image);
+				$row[] = $value->m_product_id;
+				$row[] = $value->principal;
+				$row[] = $value->md_category1;
+				$row[] = $value->md_category2;
+				$row[] = $value->md_category3;
+				$row[] = $value->color;
+				$row[] = $value->weight;
+				$row[] = $value->width;
+				$row[] = $value->height;
+				$row[] = $value->depth;
+				$row[] = $value->volume;
+				$row[] = active($value->visible);
+				$row[] = active($value->isactive);
+				$row[] = '<center>
 						<a class="btn" onclick="Edit(' . "'" . $ID . "'" . ')" title="Edit"><i class="far fa-edit text-info"></i></a>
 						<a class="btn" onclick="Destroy(' . "'" . $ID . "'" . ')" title="Delete"><i class="fas fa-trash-alt text-danger"></i></a>
 					</center>';
-			$data[] = $row;
-		endforeach;
+				$data[] = $row;
+			endforeach;
 
-		$result = array('data' => $data);
-		return json_encode($result);
+			$result = [
+				'draw' => $request->getPost('draw'),
+				'recordsTotal' => $product->countAll(),
+				'recordsFiltered' => $product->countFiltered(),
+				'data' => $data
+			];
+
+			return json_encode($result);
+		}
 	}
 
 	public function create()
 	{
 		$validation = \Config\Services::validation();
+		$request = Services::request();
 		$eProduct = new \App\Entities\Product();
 
-		$product = new M_Product();
-		$image = new M_Image();
+		$product = new M_Product($request);
+		$principal = new M_Principal();
 
 		$post = $this->request->getVar();
-		$file = $this->request->getFile('md_image_id');
-		$imgName = '';
-		$newfilename = '';
-
-		if (!empty($file)) {
-			$imgName = $file->getName();
-
-			// Renaming file before upload
-			$temp = explode(".", $imgName);
-			$newfilename = round(microtime(true)) . '.' . end($temp);
-		}
-
-		// Mapping to property
-		$post['md_image_id'] = $newfilename;
-
-		$slug = url_title($post['title'], '-', true);
+		$file = $this->request->getFile('url');
 
 		try {
-			$eProduct->fill($post);
-			$eProduct->isactive = setCheckbox(isset($post['isactive']));
-			$eProduct->slug = $slug;
-
 			if (!$validation->run($post, 'product')) {
 				$response =	$this->field->errorValidation($this->table);
 			} else {
-				$image_id = $image->insert_image($newfilename, $this->path_folder);
+				$newfilename = '';
 
-				if (isset($image_id)) {
-					$eProduct->md_image_id = $image_id;
+				if (!empty($file)) {
+					$imgName = $file->getName();
+
+					// Renaming file before upload
+					$temp = explode(".", $imgName);
+					$priData = $principal->find($post['md_principal_id']);
+					$newfilename = strtolower($priData->name) . '_' . $post['code'] . '.' . end($temp);
+					$post['url'] = $newfilename;
 				}
+
+				$eProduct->fill($post);
+				$eProduct->visible = setCheckbox(isset($post['visible']));
+				$eProduct->isactive = setCheckbox(isset($post['isactive']));
 
 				$result = $product->save($eProduct);
 
 				// Move to folder
 				$file->move($this->path_folder, $newfilename);
+
 				$response = message('success', true, $result);
 			}
 		} catch (\Exception $e) {
@@ -110,108 +138,126 @@ class Product extends BaseController
 
 	public function show($id)
 	{
-		$product = new M_Product();
-		$list = $product->detail('md_product_id', $id);
-		$reponse = $this->field->store($this->table, $list->getResult(), $list);
-		return json_encode($reponse);
+		$request = Services::request();
+		$product = new M_Product($request);
+		$list = $product->detail('md_product.md_product_id', $id, $this->path_folder);
+		$response = $this->field->store($this->table, $list->getResult(), $list);
+		return json_encode($response);
 	}
 
 	public function edit()
 	{
 		$validation = \Config\Services::validation();
+		$request = Services::request();
 		$eProduct = new \App\Entities\Product();
-		$product = new M_Product();
-		$image = new M_Image();
 
-		$image_id = 0;
+		$product = new M_Product($request);
+		$principal = new M_Principal();
 
 		$post = $this->request->getVar();
-		$row = $product->detail('md_product_id', $post['id'])->getRow();
+		$file = $this->request->getFile('url');
 
-		$file = $this->request->getFile('md_image_id');
 		$imgName = '';
 		$newfilename = '';
+		$rules = [];
+
+		$rules = [
+			'code' => [
+				'rules' 	=>	'required|is_unique[md_product.code,md_product_id,{id}]',
+				'errors' 	=> [
+					'is_unique' => 'This {field} already exists.'
+				]
+			],
+			'name' => [
+				'rules' 	=>	'required|is_unique[md_product.name,md_product_id,{id}]',
+				'errors' 	=> [
+					'is_unique' => 'This {field} already exists.'
+				]
+			],
+			'm_product_id' => [
+				'label'		=> 'iDempiere code',
+				'rules' 	=>	'required|is_unique[md_product.m_product_id,md_product_id,{id}]',
+				'errors' 	=> [
+					'is_unique' => 'This {field} already exists.'
+				]
+			],
+			'md_uom_id' => [
+				'label'		=> 	'uom',
+				'rules'		=>	'required'
+			],
+			'md_principal_id' => [
+				'label'		=> 	'principal',
+				'rules'		=>	'required'
+			],
+			'description' => [
+				'rules'		=>	'required'
+			],
+			'height' => [
+				'rules'		=>	'max_length[7]'
+			],
+			'weight' => [
+				'rules'		=>	'max_length[7]'
+			],
+			'depth' => [
+				'rules'		=>	'max_length[7]'
+			],
+			'width' => [
+				'rules'		=>	'max_length[7]'
+			],
+			'volume' => [
+				'rules'		=>	'max_length[7]'
+			]
+		];
 
 		if (!empty($file)) {
 			$imgName = $file->getName();
 
 			// Renaming file before upload
 			$temp = explode(".", $imgName);
-			$newfilename = round(microtime(true)) . '.' . end($temp);
+			$priData = $principal->find($post['md_principal_id']);
+			$newfilename = strtolower($priData->name) . '_' . $post['code'] . '.' . end($temp);
 		}
-
-		$validation->setRules([
-			'title' => [
-				'label'		=> 'product title',
-				'rules' 	=> 'required'
-			],
-			'content' => [
-				'label'		=> 'product content',
-				'rules' 	=> 'required'
-			],
-			'product_date' => [
-				'label'		=> 'posted date',
-				'rules' 	=> 'required'
-			]
-		]);
 
 		// Check if upload new image
 		if (!empty($imgName)) {
-			$post['md_image_id'] = $newfilename;
-
-			$validation->setRules([
-				'md_image_id' => [
-					'label'		=>	'image',
-					'rules'		=>	'max_size[md_image_id, 1024]|is_image[md_image_id]'
-				]
-			]);
+			$rules['url'] = [
+				'label'		=>	'product image',
+				'rules'		=>	'max_size[url, 3024]|is_image[url]|mime_in[url,image/jpg,image/jpeg,image/png]'
+			];
+			$post['url'] = $newfilename;
+		} else if (!empty($post['url'])) {
+			$post['url'];
 		} else {
-			// Check empty image post
-			if (!empty($post['md_image_id'])) {
-				$image_id = $row->image_id;
-			} else {
-				$validation->setRules([
-					'md_image_id' => [
-						'label'		=>	'image',
-						'rules'		=>	'uploaded[md_image_id]|max_size[md_image_id, 1024]|is_image[md_image_id]|mime_in[md_image_id,image/jpg,image/jpeg,image/png]'
-					]
-				]);
-				$image_id = $post['md_image_id'];
-			}
+			$rules['url'] = [
+				'label'		=>	'product image',
+				'rules'		=>	'uploaded[url]|max_size[url, 3024]|is_image[url]|mime_in[url,image/jpg,image/jpeg,image/png]'
+			];
 		}
 
-		$slug = url_title($post['title'], '-', true);
+		$validation->setRules($rules);
 
 		try {
 			$eProduct->fill($post);
 			$eProduct->md_product_id = $post['id'];
+			$eProduct->visible = setCheckbox(isset($post['visible']));
 			$eProduct->isactive = setCheckbox(isset($post['isactive']));
-			$eProduct->slug = $slug;
 
 			if (!$validation->withRequest($this->request)->run()) {
 				$response =	$this->field->errorValidation($this->table);
 			} else {
+				$row = $product->find($post['id']);
+				$oldImage = $this->path_folder . $row->url;
+
+				// Change image new upload
 				if (!empty($imgName)) {
-					// Remove old image path directory
-					unlink($this->path_folder . $row->image);
-
-					// Delete old image data
-					$delete = $image->delete($row->image_id);
-
-					if ($delete) {
-						// Insert new data image
-						$image_id = $image->insert_image($newfilename, $this->path_folder);
-
-						// Move to folder
-						$file->move($this->path_folder, $newfilename);
+					if (!empty($row->url) && file_exists($oldImage)) {
+						unlink($oldImage);
 					}
-				}
-
-				if (isset($image_id)) {
-					$eProduct->md_image_id = $image_id;
+					$file->move($this->path_folder, $newfilename);
 				}
 
 				$result = $product->save($eProduct);
+
 				$response = message('success', true, $result);
 			}
 		} catch (\Exception $e) {
@@ -223,39 +269,33 @@ class Product extends BaseController
 
 	public function destroy($id)
 	{
-		$product = new M_Product();
-		$image = new M_Image();
-
-		$image_id = 0;
-
-		$row = $product->detail('md_product_id', $id)->getRow();
-
-		if (!empty($row->image_id)) {
-			$image_id = $row->image_id;
-		}
+		$request = Services::request();
+		$product = new M_Product($request);
 
 		try {
-			// Remove image path directory
-			$unlink = unlink($this->path_folder . $row->image);
+			$row = $product->find($id);
+			$oldImage = $this->path_folder . $row->url;
 
-			if ($unlink) {
-				$image->delete($image_id);
-			}
+			if (!empty($row->url) && file_exists($oldImage))
+				unlink($this->path_folder . $row->url);
+
 			$result = $product->delete($id);
 			$response = message('success', true, $result);
 		} catch (\Exception $e) {
 			$response = message('error', false, $e->getMessage());
 		}
+
 		return json_encode($response);
 	}
 
-	public function showBy($code)
+	public function getCategory()
 	{
-		$product = new M_Product();
+		$category = new M_Category();
+		$post = $this->request->getVar();
 
 		try {
-			$result = $product->where('code', $code)->findAll();
-			$response = message('success', true, $result);
+			$result = $category->getDetail($post['principal'], $post['category']);
+			$response = message('success', true, $result->getResult());
 		} catch (\Exception $e) {
 			$response = message('error', false, $e->getMessage());
 		}
