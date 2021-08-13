ac543c7d97c6cb115c88c29b2bdaae55ab5eb095
Tintapen <okiper12@gmail.com>
2021-08-13 09:06:09 +0700

    Update view product

--- app/Views/backend/product/v_product.php
+++ app/Views/backend/product/v_product.php
@@ -4,14 +4,26 @@
 
 <?= $this->include('backend/product/form_product'); ?>
 <div class="card-body card-main">
-    <table class="table table-bordered table-hover table-pointer tb_display table-md" style="width: 100%">
+    <table class="table table-border table-hover table-pointer tb_product" style="width: 100%">
         <thead>
             <tr>
                 <th>ID</th>
                 <th>No</th>
-                <th>Title</th>
+                <th>Code</th>
+                <th>Name</th>
                 <th>Image</th>
-                <th>Posted</th>
+                <th>iDempiere Code</th>
+                <th>Principal</th>
+                <th>Category 1</th>
+                <th>Category 2</th>
+                <th>Category 3</th>
+                <th>Color</th>
+                <th>Weight</th>
+                <th>Width</th>
+                <th>Height</th>
+                <th>Depth</th>
+                <th>Volume</th>
+                <th>Visible</th>
                 <th>Active</th>
                 <th>Actions</th>
             </tr>
