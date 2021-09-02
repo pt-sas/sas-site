bbfd5ec39cc916444dbc19d180f7f4648db9adb9
Tintapen <okiper12@gmail.com>
2021-09-02 16:58:25 +0700

    Add function render_page in lib for check file

--- app/Libraries/Template.php
+++ app/Libraries/Template.php
@@ -6,6 +6,29 @@ class Template
 {
     public function render($template = '', $view_data = [])
     {
+        $view_data['filter'] = $this->render_page($template, 'form_filter');
         return view($template, $view_data);
+    }
+
+    private function render_page($path, $fileName)
+    {
+        $ext = '.php';
+        $view = explode('/', $path);
+
+        // Remove last element array
+        array_pop($view);
+
+        $path = implode('/', $view);
+
+        $dir = APPPATH . '/Views/' . $path . '/';
+
+        $file = $dir . $fileName . $ext;
+
+        if (file_exists($file))
+            $result = $path . '/' . $fileName;
+        else
+            $result = false;
+
+        return $result;
     }
 }
