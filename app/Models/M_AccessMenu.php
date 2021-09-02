a853c43de91410ce1224a12f9767c3910a21201d
Tintapen <okiper12@gmail.com>
2021-09-02 08:30:53 +0700

    Add new models and entities Access Menu

---
+++ app/Models/M_AccessMenu.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class M_AccessMenu extends Model
+{
+  protected $table      = 'sys_access_menu';
+  protected $primaryKey = 'sys_access_menu_id';
+  protected $allowedFields = [
+    'sys_role_id',
+    'sys_menu_id',
+    'sys_submenu_id',
+    'isview',
+    'iscreate',
+    'isupdate',
+    'isdelete',
+  ];
+  protected $useTimestamps = true;
+  protected $returnType = 'App\Entities\Accessmenu';
+
+  public function create($post)
+  {
+    $db = \Config\Database::connect();
+    $builder = $db->table($this->table);
+
+    foreach (json_decode($post['roles']) as $value) :
+      $data = [];
+      $data['sys_role_id'] = $post['sys_role_id'];
+      $data['isactive'] = setCheckbox(isset($post['isactive']));
+
+      if ($value->menu === 'parent') {
+        $data['sys_menu_id'] = $value->menu_id;
+        $data['sys_submenu_id'] = 0;
+      } else {
+        $data['sys_menu_id'] = $value->menu;
+        $data['sys_submenu_id'] = $value->menu_id;
+      }
+
+      $data['isview'] = $value->view;
+      $data['iscreate'] = $value->create;
+      $data['isupdate'] = $value->update;
+      $data['isdelete'] = $value->delete;
+
+      if (isset($post['id']) && !empty($value->access_id)) {
+        $result = $builder->where('sys_access_menu_id', $value->access_id)->update($data);
+      } else {
+        $result = $builder->insert($data);
+      }
+    endforeach;
+
+    return $result;
+  }
+}
