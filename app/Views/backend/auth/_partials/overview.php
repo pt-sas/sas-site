c374ab82e97199414c856759f16607b411b8db04
Tintapen <okiper12@gmail.com>
2021-09-21 16:48:42 +0700

    Update overview auth

--- app/Views/backend/auth/_partials/overview.php
+++ app/Views/backend/auth/_partials/overview.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-    <?= $this->include('backend/_partials/head') ?>
-    <body class="login">
-      <?= $this->renderSection('content') ?>
-      <?= $this->include('backend/_partials/js') ?>
-    </body>
+<?= $this->include('backend/_partials/head') ?>
+
+<body class="login">
+  <?= $this->renderSection('content') ?>
+  <?= $this->include('backend/_partials/js') ?>
+</body>
+
 </html>
