3500afcac395cc42a2757ef7ed2c485369e156ae
Tintapen <okiper12@gmail.com>
2021-10-28 17:02:30 +0700

    Update link instagram

--- app/Views/frontend/_partials/sidebar.php
+++ app/Views/frontend/_partials/sidebar.php
@@ -19,7 +19,7 @@
       <?= lang("Sidebar.MenuP2") ?>
     </p>
     <!-- <a href="javascript:void(0);" onclick="window.open('https://www.facebook.com/pages/PT.%20Sahabat%20Abadi%20Sejahtera/268648619839788/', '_blank')" class="foot-socmed"><i class="fa fa-facebook-f"></i></a> -->
-    <a href="javascript:void(0);" onclick="window.open('https://www.instagram.com/explore/locations/317814588/ptsahabat-abadi-sejahtera-philips-indonesia-sunter', '_blank')" class="foot-socmed"><i class="fa fa-instagram"></i></a>
+    <a href="javascript:void(0);" onclick="window.open('https://www.instagram.com/sahabatabadi_official', '_blank')" class="foot-socmed"><i class="fa fa-instagram"></i></a>
     <a href="javascript:void(0);" onclick="window.open('https://www.linkedin.com/company/sahabat-abadi-sejahtera', '_blank')" class="foot-socmed"><i class="fa fa-linkedin"></i></a>
     <a href="javascript:void(0);" onclick="window.open('https://www.youtube.com/channel/UCeB2XhHrFdFD3P1cw3Q9swg', '_blank')" class="foot-socmed"><i class="fa fa-youtube"></i></a>
   </div>
