554093fd4c80f0dfa3ecfc11340c30888e0e8c23
Tintapen <okiper12@gmail.com>
2023-01-11 10:46:40 +0700

    Modify footer dynamic year

--- app/Views/frontend/_partials/footer.php
+++ app/Views/frontend/_partials/footer.php
@@ -1,5 +1,5 @@
 <!-- footer -->
-<p>© Copyright 2021 PT Sahabat Abadi Sejahtera - All rights reserved.</p>
+<p>© Copyright <?= date('Y') ?> PT Sahabat Abadi Sejahtera - All rights reserved.</p>
 <!-- <a href="javascript:void(0);" onclick="window.open('https://www.facebook.com/pages/PT.%20Sahabat%20Abadi%20Sejahtera/268648619839788/', '_blank')" class="foot-socmed"><i class="fa fa-facebook-f"></i></a> -->
 <a href="javascript:void(0);" onclick="window.open('https://www.youtube.com/channel/UCeB2XhHrFdFD3P1cw3Q9swg', '_blank')" class="foot-socmed"><i class="fa fa-youtube"></i></a>
 <a href="javascript:void(0);" onclick="window.open('https://www.linkedin.com/company/sahabat-abadi-sejahtera', '_blank')" class="foot-socmed"><i class="fa fa-linkedin"></i></a>
